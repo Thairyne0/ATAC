99b342a61493f78eaf084dbdde52c00031bda7b9
Vale-n-tina <valentia.grg@gmail.com>
2025-01-27 16:54:55 +0100

    .

--- src/main/java/org/example/Biglietto.java
+++ src/main/java/org/example/Biglietto.java
@@ -1,16 +1,87 @@
 package org.example;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
-
+@Entity
+@Table(name = "Biglietto")
 public class Biglietto {
-
-    private int idBiglietto;
-    private int idUtente;
-    private int idEmissione;
-    private int idMezzo;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int IDBiglietto;
+    private int IDUtente;
+    private int IDEmissione;
+    private int IDMezzo;
+    @Column(name = "DataEmissione")
     private LocalDate dataEmissione;
+    @Column(name = "Scadenza")
     private LocalDate scadenza;
+    @Column(name = "BoolVidimato")
     private boolean vidimato;
 
+    public Biglietto(int IDUtente, boolean vidimato, LocalDate scadenza, LocalDate dataEmissione, int IDMezzo, int IDEmissione) {
+        this.IDUtente = IDUtente;
+        this.vidimato = vidimato;
+        this.scadenza = scadenza;
+        this.dataEmissione = dataEmissione;
+        this.IDMezzo = IDMezzo;
+        this.IDEmissione = IDEmissione;
+    }
+
+    public LocalDate getScadenza() {
+        return scadenza;
+    }
+
+    public void setScadenza(LocalDate scadenza) {
+        this.scadenza = scadenza;
+    }
+
+    public boolean isVidimato() {
+        return vidimato;
+    }
+
+    public void setVidimato(boolean vidimato) {
+        this.vidimato = vidimato;
+    }
+
+    public LocalDate getDataEmissione() {
+        return dataEmissione;
+    }
+
+    public void setDataEmissione(LocalDate dataEmissione) {
+        this.dataEmissione = dataEmissione;
+    }
+
+    public int getIDMezzo() {
+        return IDMezzo;
+    }
+
+    public void setIDMezzo(int IDMezzo) {
+        this.IDMezzo = IDMezzo;
+    }
+
+    public int getIDEmissione() {
+        return IDEmissione;
+    }
+
+    public void setIDEmissione(int IDEmissione) {
+        this.IDEmissione = IDEmissione;
+    }
+
+    public int getIDUtente() {
+        return IDUtente;
+    }
+
+    public void setIDUtente(int IDUtente) {
+        this.IDUtente = IDUtente;
+    }
+
+    public int getIDBiglietto() {
+        return IDBiglietto;
+    }
+
+    public void setIDBiglietto(int IDBiglietto) {
+        this.IDBiglietto = IDBiglietto;
+    }
 }
