9a1d8a42c178db7b2c123469d68b6995db178be2
Vale-n-tina <valentia.grg@gmail.com>
2025-01-27 16:35:25 +0100

    .

---
+++ src/main/java/org/example/Biglietto.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.time.LocalDate;
+
+
+public class Biglietto {
+
+    private int idBiglietto;
+    private int idUtente;
+    private int idEmissione;
+    private int idMezzo;
+    private LocalDate dataEmissione;
+    private LocalDate scadenza;
+    private boolean vidimato;
+
+}
