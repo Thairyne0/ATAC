8b73394df75fffbd5639dc66106a9010f9a17da1
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-01-28 10:41:30 +0100

    classi

--- src/main/java/org/example/MezziDiTrasporto/Mezzi.java
+++ src/main/java/org/example/MezziDiTrasporto/Mezzi.java
@@ -1,6 +1,45 @@
 package org.example.MezziDiTrasporto;
 
+import jakarta.persistence.*;
 
+@Entity
+@Table (name = "Mezzi")
 public class Mezzi {
-    
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long IDMezzo;
+
+    private int IdTratta;
+    private String TipoMezzo;
+    private String Capienza;
+
+    public Mezzi (){}
+
+    public int getIdTratta() {
+        return IdTratta;
+    }
+
+    public void setIdTratta(int idTratta) {
+        IdTratta = idTratta;
+    }
+
+    public String getTipoMezzo() {
+        return TipoMezzo;
+    }
+
+    public void setTipoMezzo(String tipoMezzo) {
+        TipoMezzo = tipoMezzo;
+    }
+
+    public String getCapienza() {
+        return Capienza;
+    }
+
+    public void setCapienza(String capienza) {
+        Capienza = capienza;
+    }
+
+    public Long getIDMezzo() {
+        return IDMezzo;
+    }
 }
--- src/main/java/org/example/MezziDiTrasporto/Tratta.java
+++ src/main/java/org/example/MezziDiTrasporto/Tratta.java
@@ -1,5 +1,72 @@
 package org.example.MezziDiTrasporto;
 
-public class Tratta {
+import jakarta.persistence.*;
 
+import java.time.Duration;
+
+@Entity
+@Table (name = "Tratta")
+public class Tratta {
+    @Id
+    @GeneratedValue
+    private Long IDTratta;
+
+    private String ZonaDiPartenza;
+    private String Capolinea;
+    private Duration TempoPrevisto;
+    private int NumeroDiViete;
+    private Duration TempoEffettivo;
+    private Duration TempoMedio;
+
+    public Long getIDTratta() {
+        return IDTratta;
+    }
+
+    public String getZonaDiPartenza() {
+        return ZonaDiPartenza;
+    }
+
+    public void setZonaDiPartenza(String zonaDiPartenza) {
+        ZonaDiPartenza = zonaDiPartenza;
+    }
+
+    public String getCapolinea() {
+        return Capolinea;
+    }
+
+    public void setCapolinea(String capolinea) {
+        Capolinea = capolinea;
+    }
+
+    public Duration getTempoPrevisto() {
+        return TempoPrevisto;
+    }
+
+    public void setTempoPrevisto(Duration tempoPrevisto) {
+        TempoPrevisto = tempoPrevisto;
+    }
+
+    public int getNumeroDiViete() {
+        return NumeroDiViete;
+    }
+
+    public void setNumeroDiViete(int numeroDiViete) {
+        NumeroDiViete = numeroDiViete;
+    }
+
+    public Duration getTempoEffettivo() {
+        return TempoEffettivo;
+    }
+
+    public void setTempoEffettivo(Duration tempoEffettivo) {
+        TempoEffettivo = tempoEffettivo;
+    }
+
+    public Duration getTempoMedio() {
+        return TempoMedio;
+    }
+
+    public void setTempoMedio(Duration tempoMedio) {
+        TempoMedio = tempoMedio;
+    }
 }
